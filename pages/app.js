366fb50b1be9e1a3ed76a844040dd26cdd9e8342
Marin <marin.knezevic1et2@gmail.com>
2021-11-01 22:58:16 +0100

    init

---
+++ pages/app.js
@@ -0,0 +1,12 @@
+//creating instance
+//goal is to control certain part of our app
+new Vue({
+    //string that tells us which elements on the page will this instance control
+    //controls everything thats inside said div element
+    el:'#vue-app',
+
+    //there goes all of our data that we store for this vue instance
+    data:{
+        name:'Film'
+    }
+});
